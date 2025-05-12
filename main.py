6d7c4117044c9386bdb02bd9827836b287ee9cc2
eabranco <ewertonbranco@gmail.com>
2025-05-12 19:29:31 -0300

    Criação da tela em branco

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+running = True
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
